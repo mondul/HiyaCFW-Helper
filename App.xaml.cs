ab807aefbee0a68b1c561149cc2e889a944156cb
Armando G. Mondul <mondul@huyzona.com>
2020-08-20 15:17:49 -0500

    Avoid start if 7-Zip is not installed

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
 
 namespace HiyaCFW_Helper
 {
@@ -7,5 +10,47 @@ namespace HiyaCFW_Helper
     /// </summary>
     public partial class App : Application
     {
+        public static string _7z;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Search for 7-Zip in the 64-bit registry
+            RegistryKey HKLM = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey _7zKey = HKLM.OpenSubKey(@"SOFTWARE\7-Zip");
+
+            if (_7zKey == null)
+            {
+                HKLM.Close();
+
+                // If not found, search for 7-Zip in the 32-bit registry
+                HKLM = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                _7zKey = HKLM.OpenSubKey(@"SOFTWARE\7-Zip");
+
+                if (_7zKey == null)
+                {
+                    HKLM.Close();
+                    MessageBox.Show("7-Zip not found. Please download it from https://www.7-zip.org/download.html", "Error - HiyaCFW Helper", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Environment.Exit(1);
+                }
+
+                else
+                {
+                    _7z = _7zKey.GetValue("Path").ToString();
+                    _7zKey.Close();
+                    HKLM.Close();
+                }
+            }
+
+            else
+            {
+                _7z = _7zKey.GetValue("Path").ToString();
+                _7zKey.Close();
+                HKLM.Close();
+            }
+
+            _7z = Path.Combine(_7z, "7z.exe");
+
+            new MainWindow().Show();
+        }
     }
 }
