a15efe099f8ad4e469043b4483761540baa26d19
Armando G. Mondul <mondul@huyzona.com>
2020-08-19 22:36:16 -0500

    Add about entry in system menu
    Code taken from https://brianlagunas.com/append-menu-items-to-the-wpf-window-contextmenu/
    Also set version number to 4.0.0.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Forms;
-using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using System.Windows.Interop;
 
 namespace HiyaCFW_Helper
 {
@@ -10,11 +12,67 @@ namespace HiyaCFW_Helper
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Win32 API methods for gaining access to the WPF Window’s system ContextMenu and inserting our custom item into it
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        private static extern bool InsertMenu(IntPtr hMenu, Int32 wPosition, Int32 wFlags, Int32 wIDNewItem, string lpNewItem);
+
+        //A window receives this message when the user chooses a command from the Window menu, or when the user chooses the maximize button, minimize button, restore button, or close button.
+        private const Int32 WM_SYSCOMMAND = 0x112;
+
+        //Draws a horizontal dividing line.This flag is used only in a drop-down menu, submenu, or shortcut menu.The line cannot be grayed, disabled, or highlighted.
+        private const Int32 MF_SEPARATOR = 0x800;
+
+        //Specifies that an ID is a position index into the menu and not a command ID.
+        private const Int32 MF_BYPOSITION = 0x400;
+
+        //Specifies that the menu item is a text string.
+        private const Int32 MF_STRING = 0x0;
+
+        //Menu Id for our custom menu item.
+        private const Int32 _AboutMenuItemId = 1000;
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
         }
 
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr windowhandle = new WindowInteropHelper(this).Handle;
+            HwndSource hwndSource = HwndSource.FromHwnd(windowhandle);
+
+            //Get the handle for the system menu
+            IntPtr systemMenuHandle = GetSystemMenu(windowhandle, false);
+
+            //Insert our custom menu item after Maximize
+            InsertMenu(systemMenuHandle, 5, MF_BYPOSITION | MF_SEPARATOR, 0, string.Empty); //Add a menu seperator
+            InsertMenu(systemMenuHandle, 6, MF_BYPOSITION, _AboutMenuItemId, "About HiyaCFW Helper"); //Add the about menu item
+
+            hwndSource.AddHook(new HwndSourceHook(WndProc));
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Check if the SystemCommand message has been executed
+            if (msg == WM_SYSCOMMAND)
+            {
+                //check which menu item was clicked
+                switch (wParam.ToInt32())
+                {
+                    case _AboutMenuItemId:
+                        MessageBox.Show($"HiyaCFW Helper\r\nVersion {Assembly.GetExecutingAssembly().GetName().Version}\r\n\r\nhttps://github.com/mondul",
+                            "About HiyaCFW Helper", MessageBoxButton.OK, MessageBoxImage.Information);
+                        handled = true;
+                        break;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
 
         private void EnableStartBtn()
         {
@@ -36,7 +94,7 @@ namespace HiyaCFW_Helper
 
         private void browseDriveBtn_Click(object sender, RoutedEventArgs e)
         {
-            FolderBrowserDialog folderDialog = new FolderBrowserDialog
+            System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog
             {
                 Description = "Please choose your SD card drive path or an output folder for the custom firmware files:"
             };
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
